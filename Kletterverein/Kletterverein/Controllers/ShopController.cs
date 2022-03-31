f6a7248e8bb7d992b061ef9ba637e5c7d5e70038
NiklasSillaber <91011800+NiklasSillaber@users.noreply.github.com>
2022-03-31 11:29:31 +0200

    SHOP - finish:))))

--- Kletterverein/Kletterverein/Controllers/ShopController.cs
+++ Kletterverein/Kletterverein/Controllers/ShopController.cs
@@ -13,12 +13,28 @@ namespace Kletterverein.Controllers
         {
             //Alle Producte aus der DB lesen und mitgeben
 
-            Product pTest1 = new Product(1, "Mein SChuhe", "Toll", Brand.Nike, 110.3m);
+            Product pTest1 = new Product(1, "SCHU", "Toll", Brand.Nike, 110.3m);
             Product pTest2 = new Product(2, "Deine Shu", "sad", Brand.Nike, 100.3m);
+            Product pTest3 = new Product(3, "Mein SChuhe", "Toll", Brand.Nike, 110.3m);
+            Product pTest4 = new Product(4, "Deine Shu", "sad", Brand.Nike, 100.3m);
+            Product pTest5 = new Product(5, "Mein SChuhe", "Toll", Brand.Nike, 110.3m);
+            Product pTest6 = new Product(6, "Deine Shu", "sad", Brand.Nike, 100.3m);
+            Product pTest7 = new Product(7, "Mein SChuhe", "Toll", Brand.Nike, 110.3m);
+            Product pTest8 = new Product(8, "Deine Shu", "sad", Brand.Nike, 100.3m);
+            Product pTest9 = new Product(9, "Mein SChuhe", "Toll", Brand.Nike, 110.3m);
+            Product pTest10 = new Product(10, "Deine Shu", "sad", Brand.Nike, 100.3m);
 
             List<Product> myProducts = new List<Product>();
             myProducts.Add(pTest1);
             myProducts.Add(pTest2);
+            myProducts.Add(pTest3);
+            myProducts.Add(pTest4);
+            myProducts.Add(pTest5);
+            myProducts.Add(pTest6);
+            myProducts.Add(pTest7);
+            myProducts.Add(pTest8);
+            myProducts.Add(pTest9);
+            myProducts.Add(pTest10);
 
             return View(myProducts);
         }
